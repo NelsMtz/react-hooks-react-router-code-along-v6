ad9f3331be2a99d41e547937b129cce135039668
Nelly <nmatu308@gmail.com>
2024-08-08 22:44:07 +0300

    forst-commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import routes from "./routes";
 import Home from "./pages/Home";
+
+const router = createBrowserRouter(routes)
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<Home />);
